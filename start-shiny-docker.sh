9d1b3f74277d519ab3af2df154ea84470c978076
Fernando Cagua <efcaguab@gmail.com>
2018-04-02 19:23:22 +1200

    load stuff for demografia del voto app

--- start-shiny-docker.sh
+++ start-shiny-docker.sh
@@ -5,15 +5,15 @@ docker run --name shiny -d -p 3838:3838 \
     -v ~/github/cagua-website/cagua_shiny/logs:/var/log/shiny-server/ \
     rocker/shiny
 
-# docker exec -it mywebsite apt-get update
-# docker exec -it mywebsite sudo apt-get install -y libxml2-dev libssl-dev
-# docker exec -it mywebsite Rscript -e "install.packages('packrat')"
+docker exec -it shiny apt-get update
+docker exec -it shiny sudo apt-get install -y libxml2-dev libssl-dev
+docker exec -it shiny Rscript -e "install.packages('packrat')"
 
 # data-rich-insight-poor-live-demo
-# docker exec -it mywebsite sudo chown -R shiny:shiny /srv/shiny-server/data-rich-live-demo
-# docker exec -it mywebsite Rscript -e "packrat::restore('/srv/shiny-server/data-rich-live-demo')"
+# docker exec -it shiny sudo chown -R shiny:shiny /srv/shiny-server/data-rich-live-demo
+# docker exec -it shiny Rscript -e "packrat::restore('/srv/shiny-server/data-rich-live-demo')"
 
 # demografia del voto app
-# docker exec -it mywebsite sudo chown -R shiny:shiny /srv/shiny-server/demografia-del-voto
-# docker exec -it mywebsite Rscript -e "packrat::restore('/srv/shiny-server/demografia-del-voto')"
-# docker exec -it mywebsite make -C /srv/shiny-server/demografia-del-voto
+docker exec -it shiny sudo chown -R shiny:shiny /srv/shiny-server/demografia-del-voto
+docker exec -it shiny Rscript -e "packrat::restore('/srv/shiny-server/demografia-del-voto')"
+docker exec -it shiny make -C /srv/shiny-server/demografia-del-voto
